f2bb613446934f081e3062bac1106a6a896efcc7
Sumin Park <clappingmin@gmail.com>
2022-02-18 21:57:52 +0900

    index.js 수정

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './shared/App';
-import {Provider} from "react-redux";
+import { Provider } from 'react-redux';
 
 // 스토어 주입
-import store from "./redux/configureStore"
-
 import store from './redux/configureStore';
-import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <Provider store={store}>
