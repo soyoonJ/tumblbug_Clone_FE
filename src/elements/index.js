8d2f48386be59d6fbc11884c374951b25d7857cf
Sumin Park <clappingmin@gmail.com>
2022-02-18 23:39:23 +0900

    css 초기설정 추가

--- src/elements/index.js
+++ src/elements/index.js
@@ -1,9 +1,10 @@
 // element안에 있는 컴포넌트를 한번에 합쳐서 다른 곳에서 한번에 불러오기 위함
 
-import Grid from "./Grid";
-import Image from "./Image"
-import Text from "./Text";
-import Input from "./Input";
-import Button from "./Button";
+import Grid from './Grid';
+import Image from './Image';
+import Text from './Text';
+import Input from './Input';
+import Button from './Button';
+import Checkbox from './Checkbox';
 
-export {Grid, Image, Text, Input, Button};
+export { Grid, Image, Text, Input, Button, Checkbox };
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,12 +1,33 @@
 import React from 'react';
-import { Grid } from '../elements';
+import styled from 'styled-components';
+import { Grid, Text, Input, Button, Checkbox } from '../elements';
 
 const Signup = () => {
   return (
     <React.Fragment>
-      <Grid padding="32px"></Grid>
+      <Grid padding="32px">
+        <h1>이메일로 가입하기</h1>
+        <Grid>
+          <form>
+            <Input label="이름" placeholder="사용하실 이름을 입력해주세요." />
+            <Input
+              label="이메일 주소"
+              placeholder="이메일 주소를 입력해주세요."
+            />
+            <Input placeholder="이메일 주소를 확인합니다." />
+            <Input label="비밀번호" placeholder="비밀번호를 입력해주세요." />
+            <Input placeholder="비밀번호를 확인합니다." />
+            <Checkbox text="전체동의" />
+            <Button text="가입하기" />
+          </form>
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 };
+
+const Line = styled.div`
+  width: 100%;
+`;
 
 export default Signup;
