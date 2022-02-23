559da59d318638686b29e1756815ec3fbacccdc0
Sumin Park <clappingmin@gmail.com>
2022-02-23 21:42:42 +0900

    카카오 로그인 완성, 회원가입 경고 메시지 수정, 알럿 띄우기

---
+++ src/pages/Auth.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { useEffect } from 'react';
+import axios from 'axios';
+import qs from 'qs';
+import { useHistory } from 'react-router-dom';
+
+import { useDispatch } from 'react-redux';
+import { actionCreators as userActions } from '../redux/modules/user';
+
+const Auth = () => {
+  const dispatch = useDispatch();
+
+  const REST_API_KEY = '7b3f8ab784a724634fbb9c5250dd699e';
+  const REDIRECT_URI = 'http://localhost:3000/oauth/kakao/callback';
+  const CLIENT_SECRET = 'rf1aaPug1Mcg9hjRpjZinu6CharA7byO';
+  // calllback으로 받은 인가코드
+  const code = new URL(window.location.href).searchParams.get('code');
+  const history = useHistory();
+
+  const getToken = async () => {
+    const payload = qs.stringify({
+      grant_type: 'authorization_code',
+      client_id: REST_API_KEY,
+      redirect_uri: REDIRECT_URI,
+      code: code,
+      client_secret: CLIENT_SECRET,
+    });
+    try {
+      // access token 가져오기
+      const res = await axios.post(
+        'https://kauth.kakao.com/oauth/token',
+        payload
+      );
+
+      // Kakao Javascript SDK 초기화
+      window.Kakao.init(REST_API_KEY);
+      // access token 설정
+      window.Kakao.Auth.setAccessToken(res.data.access_token);
+      dispatch(userActions.getKakaoProfile());
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getToken();
+  }, []);
+  return null;
+};
+export default Auth;
--- src/shared/api.js
+++ src/shared/api.js
@@ -1,39 +1,41 @@
-import axios from "axios";
+import axios from 'axios';
 
-const token = localStorage.getItem("login-token");
+const token = localStorage.getItem('login-token');
 
 const api = axios.create({
-  baseURL: "http://3.35.176.155:8080/",
+  baseURL: 'http://3.35.176.155:8080/',
   headers: {
-    "content-type": "application/json;charset=UTF-8",
-    accept: "application/json,",
+    'content-type': 'application/json;charset=UTF-8',
+    accept: 'application/json,',
     Authorization: `Bearer ${token}`,
   },
 });
 
 export const apis = {
   // user
-  login: (email, password) => api.post("/api/users/login", { email, password }),
+  login: (email, password) => api.post('/api/users/login', { email, password }),
+  loginKakao: (email, nickname) =>
+    api.post('/api/users/loginKakao', { email, nickname }),
   signup: (nickname, email, password) =>
-    api.post("/api/users/signup", {
+    api.post('/api/users/signup', {
       nickname,
       email,
       password,
     }),
 
-  loginCheck: () => api.post("/api/users/me"),
+  loginCheck: () => api.post('/api/users/me'),
 
   // articles
-  mainAriticles: () => api.get("/api/articles/mainProjects"), // 주목할 만한 프로젝트
-  popularAriticles: () => api.get("/api/articles/popularProjects"), //  인기 프로젝트
-  myAriticles: () => api.get("/api/articles/myDonatedProjects"),
-  AllAriticles: () => api.get("/api/articles/all"),
-  cateAriticles: () => api.get("/api/articles/category?category=name"),
-  searchAriticles: () => api.get("/api/articles?search=keyword"),
+  mainAriticles: () => api.get('/api/articles/mainProjects'), // 주목할 만한 프로젝트
+  popularAriticles: () => api.get('/api/articles/popularProjects'), //  인기 프로젝트
+  myAriticles: () => api.get('/api/articles/myDonatedProjects'),
+  AllAriticles: () => api.get('/api/articles/all'),
+  cateAriticles: () => api.get('/api/articles/category?category=name'),
+  searchAriticles: () => api.get('/api/articles?search=keyword'),
 
-  ariticles: () => api.get("/api/articles?search=keyword"), // 상세 조회
+  ariticles: () => api.get('/api/articles?search=keyword'), // 상세 조회
   donation: () => api.fetch(`/api/article/:articleId/donation`), // 후원하기
-  donationCancel: () => api.fetch("/api/article/:articleId/donationCancel"), // 후원취소
+  donationCancel: () => api.fetch('/api/article/:articleId/donationCancel'), // 후원취소
 
   // comment
   comments: () => api.get(`/api/comments/:articleId`),
